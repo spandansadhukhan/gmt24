6cdadf44fe8bf1f5e738aaabe202a0164ec61447
bikash_mondal <nits.bikash@gmail.com>
2018-07-10 11:02:59 +0530

    10_07morning

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,5 @@
 
 export const Config = {
-    baseUrl:'http://111.93.169.90/team6/webshop/webservice/index.php/frontend/'
-    //baseUrl:'http://178.62.61.183/webservice/index.php/frontend/'
+    //baseUrl:'http://111.93.169.90/team6/webshop/webservice/index.php/frontend/'
+    baseUrl:'http://178.62.61.183/webservice/index.php/frontend/'
 }
