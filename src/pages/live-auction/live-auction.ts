9b971c3223c8a0300ddd48f4f740280bfdc616c7
Bikash <nits.bikash@gmail.com>
2018-09-27 17:57:16 +0530

    27_09evening

--- src/pages/live-auction/live-auction.ts
+++ src/pages/live-auction/live-auction.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams,LoadingController,ToastController } from 'ionic-angular';
+import { IonicPage, NavController, NavParams,LoadingController,ToastController,AlertController } from 'ionic-angular';
 import { AuthServiceProvider } from '../../providers/auth-service/auth-service';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 /**
  * Generated class for the LiveAuctionPage page.
@@ -15,6 +16,9 @@ import { AuthServiceProvider } from '../../providers/auth-service/auth-service';
   templateUrl: 'live-auction.html',
 })
 export class LiveAuctionPage {
+
+
+  bidForm: FormGroup;
   timeInSeconds:any;
   remainingTime: any;
   displayTime:any;
@@ -44,28 +48,72 @@ export class LiveAuctionPage {
   bidincrement:any;
   thresholdprice:any;
   baseauctionprice:any;
-
-
-
+  bidcount:any;
+  lastbidvalue:any;
+  time_difference:any;
+  bidmatch:boolean=false;
+  winnerid:any;
+  seller_id:any;
+  refreshIntervalId:any;
   constructor(public navCtrl: NavController, 
     public navParams: NavParams,
     public loadingCtrl: LoadingController,
     public authService: AuthServiceProvider,
-    public toastCtrl:ToastController,) {
+    public toastCtrl:ToastController,
+    public builder: FormBuilder,
+    public alertCtrl: AlertController,) {
 
-      this.loguser =  JSON.parse(localStorage.getItem('userData'));
+    this.loguser =  JSON.parse(localStorage.getItem('userData'));
     this.user_id=this.loguser.user_id;
+
+    this.bidForm = builder.group({
+      'bid': [null, null],
+    });
+
+    
+
+
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad LiveAuctionPage');
     this.product_id = this.navParams.get('product_id');
-    this.initTimer();
-    this. startTimer()
-  this.productsDetails(this.product_id);
+    this.productsDetails(this.product_id);
   }
 
+  ionViewWillEnter(){
 
+    this.refreshIntervalId =  setInterval(() => {
+      let serval1={
+        "product_id":this.product_id,
+        "user_id":this.user_id,
+      }
+      this.authService.postData(serval1,'ProductsDetails_app').then((result) => {
+        this.responseData = result
+        if( this.responseData.Ack == 1)
+        {
+          this.productLists =  this.responseData.productList;
+          this.currency_code=this.productLists.currency_code;
+          this.countuniquebids=this.productLists.countuniquebids;
+          this.maxbiddername=this.productLists.maxbiddername;
+          this.higestbidderbid=this.productLists.higestbidderbid;
+          this.usermaxbid=this.productLists.maxbid;
+          this.nextbidprice=this.productLists.nextbidprice;
+          this.bidincrement=this.productLists.bidincrement;
+          this.thresholdprice=this.productLists.thresholdprice;
+          this.bidcount=this.productLists.bidcount;
+          this.lastbidvalue=this.productLists.lastbidvalue;
+        }
+      });
+      }, 1000);
+  }
+
+  ionViewWillLeave(){
+
+    clearInterval(this.refreshIntervalId);
+
+  }
+  
   private presentToast(text) {
     let toast = this.toastCtrl.create({
       message: text,
@@ -75,16 +123,13 @@ export class LiveAuctionPage {
     toast.present();
   }
 
-  initTimer() {
+  initTimer(time) {
    
-    
-      this.timeInSeconds = 10000; 
-   
+    this.timeInSeconds = time;//define total seceond 
     this.runTimer = false;
     this.hasStarted = false;
     this.hasFinished = false;
     this.remainingTime = this.timeInSeconds;
-    
     this.displayTime = this.getSecondsAsDigitalClock(this.remainingTime);
   }
   
@@ -105,7 +150,7 @@ export class LiveAuctionPage {
       }
       else {
         this.hasFinished = true;
-        alert('times out')
+       // alert('times out')
       }
     }, 1000);
   }
@@ -121,7 +166,7 @@ export class LiveAuctionPage {
     hoursString = (hours < 10) ? "0" + hours : hours.toString();
     minutesString = (minutes < 10) ? "0" + minutes : minutes.toString();
     secondsString = (seconds < 10) ? "0" + seconds : seconds.toString();
-    return hoursString + ':' + minutesString + ':' + secondsString;
+    return hoursString + ' HOUR:' + minutesString + ' MINUTES:' + secondsString + ' SECONDS';
   }
 
 
@@ -140,8 +185,8 @@ export class LiveAuctionPage {
     
     this.authService.postData(serval,'ProductsDetails_app').then((result) => {
       this.responseData = result
-     //alert(this.responseData.Ack);
-     console.log('productsdetails',this.responseData);
+     
+     //console.log('productsdetails',this.responseData);
       if( this.responseData.Ack == 1)
       {
         loading.dismiss();
@@ -153,7 +198,7 @@ export class LiveAuctionPage {
         this.gender=this.productLists.gender;
         this.product_single_image=this.productLists.product_single_image;
         this.currency_code=this.productLists.currency_code;
-        
+        this.seller_id=this.productLists.seller_id;
         this.countuniquebids=this.productLists.countuniquebids;
         this.maxbiddername=this.productLists.maxbiddername;
         this.higestbidderbid=this.productLists.higestbidderbid;
@@ -162,7 +207,11 @@ export class LiveAuctionPage {
         this.bidincrement=this.productLists.bidincrement;
         this.thresholdprice=this.productLists.thresholdprice;
         this.baseauctionprice=this.productLists.baseauctionprice;
-         
+        this.bidcount=this.productLists.bidcount;
+        this.lastbidvalue=this.productLists.lastbidvalue;
+        this.time_difference=this.productLists.time_difference;
+        this.initTimer(this.time_difference);
+        this. startTimer()
       }
       else
       {
@@ -178,11 +227,149 @@ export class LiveAuctionPage {
   
 }
 
+checkinputbid(bid,nbid){
 
+    if(bid >= nbid)
+    {
+      
+     this.bidmatch = true;
+    }
+    else{
+      this.bidmatch = false;
+    }
+  
+}
 
+bidsubmit(fromdata){
+  
+  let loading = this.loadingCtrl.create({
+    content: 'Please Wait...'
+  });
+  loading.present();
+ 
+  let serval={
+    "product_id":this.product_id,
+    "user_id":this.user_id
+  }
+  
+  this.authService.postData(serval,'checkauctionvaliditybeforeaddbid').then((result) => {
+    this.responseData = result
+   
+   console.log('productsdetails',this.responseData);
+    if( this.responseData.Ack == 1)
+    {
+      this.addbid(fromdata.bid);
+      loading.dismiss();
+    }
+    else if(this.responseData.Ack == 2)
+    {
+      loading.dismiss();
+      const alert = this.alertCtrl.create({
+        title: "Congratulation!",
+        subTitle:"You Win This Auction. PLease Pay Now.",
+        buttons: ['OK']
+      });
+      alert.present();
+      this.winnerid = this.responseData.auctionwinner;
 
+    }else{
 
+      loading.dismiss();
+      const alert = this.alertCtrl.create({
+        title: "Sorry!",
+        subTitle:"Better luck next time.",
+        buttons: ['OK']
+      });
+      alert.present();
+      this.navCtrl.setRoot('HomePage');
+    }
+   
+  }, (err) => {
+    loading.dismiss();
+    console.log(err);
+    
+  });
 
+}
+
+addbid(bid){
+ 
+  let serval={
+    "productid":this.product_id,
+    "userid":this.user_id,
+    "bidprice":bid,
+    "bidincrement":this.bidincrement,
+    "currency":this.currency_code,
+    "uploaderid":this.seller_id,
+  }
+  //console.log('bidfromdata',serval);
+  this.authService.postData(serval,'bidsubmit').then((result) => {
+    this.responseData = result
+   
+    if( this.responseData.Ack == 1)
+    {
+      let serval1={
+        "product_id":this.product_id,
+        "user_id":this.user_id,
+        
+      }
+      this.authService.postData(serval1,'ProductsDetails_app').then((result) => {
+        this.responseData = result
+       
+       //console.log('productsdetails',this.responseData);
+        if( this.responseData.Ack == 1)
+        {
+          
+          this.productLists =  this.responseData.productList;
+          this.currency_code=this.productLists.currency_code;
+          this.countuniquebids=this.productLists.countuniquebids;
+          this.maxbiddername=this.productLists.maxbiddername;
+          this.higestbidderbid=this.productLists.higestbidderbid;
+          this.usermaxbid=this.productLists.maxbid;
+          this.nextbidprice=this.productLists.nextbidprice;
+          this.bidincrement=this.productLists.bidincrement;
+          this.thresholdprice=this.productLists.thresholdprice;
+          this.bidcount=this.productLists.bidcount;
+          this.lastbidvalue=this.productLists.lastbidvalue;
+
+        }
+        else
+        {
+          
+          this.productLists = '';
+        }
+       
+      }, (err) => {
+        
+        console.log(err);
+        
+      });
+
+      this.bidmatch = false;
+      this.presentToast('Bid sumitted successfully.');
+    }
+    else{
+
+      const alert = this.alertCtrl.create({
+        title: "Sorry!",
+        subTitle:"Something went wrong.",
+        buttons: ['OK']
+      });
+      alert.present();
+      
+    }
+   
+  }, (err) => {
+    console.log(err);
+    const alert = this.alertCtrl.create({
+      title: "Error!",
+      subTitle:"Error occured.",
+      buttons: ['OK']
+    });
+    alert.present();
+  });
+
+}
 
 
 }
