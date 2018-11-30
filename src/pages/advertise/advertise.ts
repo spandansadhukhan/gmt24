e5322e70c472deb70218455a6a74fc7cd115586f
arunavaguha <arunavaguha@natitsolved.com>
2018-11-30 10:15:49 +0530

    morning-30-11

--- src/pages/advertise/advertise.ts
+++ src/pages/advertise/advertise.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams,MenuController } from 'ionic-angular';
-
+import { IonicPage, NavController, NavParams,MenuController ,LoadingController} from 'ionic-angular';
+import { VideoPlayer } from '@ionic-native/video-player';
+import {AuthServiceProvider} from '../../providers/auth-service/auth-service';
 /**
  * Generated class for the AdvertisePage page.
  *
@@ -15,16 +16,85 @@ import { IonicPage, NavController, NavParams,MenuController } from 'ionic-angula
 })
 export class AdvertisePage {
 
+  public  responseData:any;
+  public  videoList:any;
+  length:any;
+  counter=0;
+  srcs:any;
+ 
+
   constructor(public navCtrl: NavController, 
     public navParams: NavParams,
-    public menu: MenuController,) {
+    public menu: MenuController,
+    //private videoPlayer: VideoPlayer,
+    public loadingCtrl: LoadingController,
+    public authService:AuthServiceProvider,
+  ) {
+   
   }
 
   ionViewDidLoad() {
     this.menu.enable(false, 'loggedOutMenu');
+    this.getvideos();
+   
     console.log('ionViewDidLoad AdvertisePage');
   }
+ 
+  getvideos(){
 
+    let loading = this.loadingCtrl.create({
+      content: 'Please Wait...'
+    });
+    
+    loading.present();
+    //this.selectedcurrency = JSON.parse(localStorage.getItem('selectedcurrency'));
+    
+    this.authService.getvideo({}).subscribe((result) => {
+     
+      this.responseData = result
+      
+      console.log('aruvideo1',this.responseData.videolist);
+      if( this.responseData.Ack == 1)
+      {
+        loading.dismiss();
+       
+        this.videoList =  this.responseData.videolist;
+        this.length = this.videoList.length;
+        
+        //alert( this.counter);
+        this.srcs = this.responseData.videolist[this.counter].video;
+       
+        //alert( this.counter)
+        //alert(this.srcs);
+        console.log('aruvideo',this.srcs);
+        
+      } else
+      {
+        loading.dismiss();
+        this.videoList ="";
+      }
+     
+    }, err => {
+      loading.dismiss();
+      console.log(err);
+      
+    });
+  }
+
+  videoEnded(){
+    if(this.length > this.counter){
+      this.counter++;
+      this.getvideos();
+    }else{
+      this.counter=0;
+      alert();
+      this.getvideos();
+    }
+    
+  }
+
+
+  
   skip() {
     this.navCtrl.push("LoginnewPage");
   }
